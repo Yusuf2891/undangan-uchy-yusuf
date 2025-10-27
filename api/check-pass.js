f62ea65166cc831750770bcbbd22afc656954f4a
Yusuf2891 <167664821+Yusuf2891@users.noreply.github.com>
2025-10-27 16:11:21 +0700

    Update check-pass.js

--- api/check-pass.js
+++ api/check-pass.js
@@ -1,6 +1,5 @@
-// CommonJS version (tidak perlu package.json "type": "module")
-module.exports = (req, res) => {
-  const pass = req.query?.pass;
-  const PASSWORD = process.env.INVITE_PASS ||
+export default function handler(req, res) {
+  const { pass } = req.query;
+  const PASSWORD = process.env.INVITE_PASS; // atur di Environment Vercel
   res.status(200).json({ valid: pass === PASSWORD });
-};
+}
